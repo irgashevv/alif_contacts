1f105a46be6f566daae5d6ad2548e0a950b90f4e
ibra <irgashev1200@gmail.com>
2021-04-07 19:16:28 +0300

    Clear Coding

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,22 +1,76 @@
-$(document).ready(function() {
-    var max_fields = 10;
-    var wrapper = $(".container1");
-    var add_button = $(".add_form_field");
-
+$(document).ready(function () {
+    var max_fields = 100;
     var x = 1;
-    $(add_button).click(function(e) {
-        e.preventDefault();
+
+    $('#plus_email').on('click', () => {
         if (x < max_fields) {
             x++;
-            $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="delete">Delete</a></div>');
+            $('#emails_input').append('<div class="d-flex mt-2"><input type="text" name="emails[]" class="form-control"/>' +
+                '<a href="#" class="delete btn btn-danger ml-2">-</a></div>')
         } else {
             alert('You Reached the limits')
         }
     });
-
-    $(wrapper).on("click", ".delete", function(e) {
+    $('#emails_input').on("click", ".delete", function(e) {
         e.preventDefault();
         $(this).parent('div').remove();
         x--;
-    })
-});
+    });
+
+    $('#plus_phone').on('click', () => {
+        if (x < max_fields) {
+            x++;
+            $('#phones_input').append('<div class="d-flex mt-2"><input type="text" name="phones[]" class="form-control"/>' +
+                '<a href="#" class="delete btn btn-danger ml-2">-</a></div>')
+        } else {
+            alert('You Reached the limits')
+        }
+    });
+    $('#phones_input').on("click", ".delete", function(e) {
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    });
+
+    $('#submit').on('click', () => {
+        var input_emails = $("input[name='input_emails[]']")
+            .map(function(){
+                return $(this).val();
+            }).get();
+        var input_phones = $("input[name='input_phones[]']")
+            .map(function(){
+                return $(this).val();
+            }).get();
+
+        var emails = $("input[name='emails[]']")
+            .map(function(index, item){
+                let id = $(this).val();
+                let email = input_emails[index];
+                if (!email) {
+                    let emailll = $('emails[' + index + "]").val()
+                    return $('emails[index]')
+                }
+                if (input_emails[index]) {
+                    // return "{'id:'" + id + "'phone:'" + email + "}";
+                    return id + " => " + email;
+                }
+            }).get();
+
+        var phones = $("input[name='phones[]']")
+            .map(function(index){
+                let id = $(this).val();
+                let phone = input_phones[index];
+                if (!phone) {
+                    return "[" + phone + "]"
+                }
+                if (input_emails[index]) {
+                    // return "{'id:'" + id + "'phone:'" + email + "}";
+                    return id + " => " + phone;
+                }
+
+            }).get();
+
+        $("input[name='phones_value']").val(phones);
+        $("input[name='emails_value']").val(emails);
+    });
+})
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include_once "src/Service/Router.php";
 
 $router = new Router();
+
 $router->index();
